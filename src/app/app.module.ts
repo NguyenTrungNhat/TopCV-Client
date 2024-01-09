fb6b39bc51e80981e30a634bc3999050cb3786ef
Vinamilk <nhatnguyen20102002@gmail.com>
2024-01-09 22:07:50 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/shared/header/header.component';
+import { FooterComponent } from './home/shared/footer/footer.component';
+import { TrangchuComponent } from './home/pages/trangchu/trangchu.component';
+import { TintuyendungComponent } from './home/pages/tintuyendung/tintuyendung.component';
+import { VieclamComponent } from './home/pages/vieclam/vieclam.component';
+import { CongtyComponent } from './home/pages/congty/congty.component';
+import { LayoutComponent } from './home/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TrangchuComponent,
+    TintuyendungComponent,
+    VieclamComponent,
+    CongtyComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
